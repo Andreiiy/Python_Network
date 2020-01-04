51b8b9fb054c89f6982fa41219170898c2c2559e
Andrei <41228222+Andreiiy@users.noreply.github.com>
2020-01-04 21:38:28 +0200

    Add files via upload

--- HTTPServer/Server.py
+++ HTTPServer/Server.py
@@ -1,25 +1,73 @@
-import socket
 
-server_socket=socket.socket()
-server_socket.bind(('0.0.0.0',8820))
-server_socket.listen(1)
-client_sock, adr = server_socket.accept()
+from socket import  *
 
-go = True
-name_client = client_sock.recv(1024)
-print "Helo my name is "+name_client
-name= raw_input("Enter Name")
-client_sock.send(name)
+#region------------  C O N S T A N T S ----------------------------
+PORT=9080                 #
+IP=""
+ADDR = (IP,PORT)
+BUF_SIZE = 2000
+VERSION = "HTTP/1.1"
+STATUS_OK = "200 OK"
+ENDLINE = "\r\n"
+#endregion
 
-while go:
-    data=client_sock.recv(1024)
-    if data == "bye":
-        go = False
-        server_socket.close()
-        client_sock.close()
-    else:
-        print name_client +":"+data
-        msg = raw_input(name +':')
-        client_sock.send(msg)
+#region------------  G L O B A L S ----------------------------
+sockListen = None
+#endregion
 
+#region------------  M E T H O D S ----------------------------
+def listen():
+    global sockListen
+    sockListen = socket()
+    sockListen.bind(ADDR)
+    sockListen.listen(1)
+    print  "Server waiting..."
+
+
+def get_file_content(filename):
+    file_index = open(filename, "rb")
+    file_content = file_index.read()
+    file_index.close()
+    return file_content
+
+def response(data):
+    response_data = ""
+    headers_items = data.split(ENDLINE)
+    if headers_items[0].startswith("GET"):
+        url = headers_items[0].split(" ")[1]
+        response_data = VERSION + " " + STATUS_OK + ENDLINE + ENDLINE
+        if url == "/":
+            file_content = get_file_content("index.html")
+        else:
+            file_content = get_file_content(url[1:])
+
+    return response_data+file_content
+
+    return  response_data
+
+def request_response():
+    global sockListen
+    exit = False
+    while True:
+        sock, addr = sockListen.accept()
+        try:
+            request_data = sock.recv(BUF_SIZE)
+            print "Client : ", addr[0]
+            print request_data
+            if request_data and len(request_data) > 0 :
+                response_data = response(request_data)
+                sock.send(response_data)
+                sock.close()
+        except error as er:
+            exit = True
+#endregion
+
+#region------------  M A I N  ----------------------------
+def main():
+    listen()
+    request_response()
+#endregion
+
+if  __name__== "__main__":
+    main()
 
